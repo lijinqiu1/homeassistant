b7c5b9238722b0d08f63ea8ff5021bf93cf41144
lijinqiu <lijinqiux@163.com>
2019-03-15 13:53:18 +0800

    完善easy控制界面

--- easy.py
+++ easy.py
@@ -9,10 +9,27 @@ Builder.load_file('data/screens/easy.kv')
 class EasyFloatLayout(FloatLayout):
     def __init__(self, **kwargs):
         super(EasyFloatLayout, self).__init__(**kwargs)
-        self.manager = ScreenManager(transition=SlideTransition(
-            duration=.15))
         self.ids.easy.current = 'mode'
-
+        self.screen_index = 0
+        self.mode_index = 0
 
     def on_main_screen(self):
         pass
+
+    def go_previous_screen(self):
+        self.ids.easy.transition.direction = 'left'
+        if self.screen_index == 0:
+            self.ids.easy.current = 'cover'
+            self.screen_index = 1
+        else:
+            self.ids.easy.current = 'mode'
+            self.screen_index = 0
+
+    def go_next_screen(self):
+        self.ids.easy.transition.direction = 'right'
+        if self.screen_index == 0:
+            self.ids.easy.current = 'cover'
+            self.screen_index = 1
+        else:
+            self.ids.easy.current = 'mode'
+            self.screen_index = 0
